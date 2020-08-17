30438d5a77fda619562255e65ebf826c0e044a08
francescobuccheri00 <69636344+francescobuccheri00@users.noreply.github.com>
2020-08-17 16:01:50 +0200

    Create printtables.hpp

---
+++ printtables.hpp
@@ -0,0 +1,32 @@
+#ifndef PRINTTABLES_HPP
+#define PRINTTABLES_HPP
+
+inline void printTables(std::vector<Seir> const &numbers) {
+  std::cout << "    (S)    "
+            << "      (E)     "
+            << "   (I)      "
+            << "  (R)     "
+            << "     R0"
+            << "\n";
+  for (auto const &n : numbers) {
+    std::cout << std::to_string(n.s) << " | " << std::to_string(n.e) << " | "
+              << std::to_string(n.i) << " | " << std::to_string(n.r) << " "
+              << " | " << std::to_string(n.r0) << "\n"
+              << "-------------------------------------------------------------"
+                 "------------------";
+  }
+  std::cout << "\n";
+  std::cout << "Reinserisci il totale di giorni che hai voluto controllare e "
+               "visualizzerai il numero massimo di esposti e infetti : ";
+  int nm;
+  std::cin >> nm;
+  auto const &i = numbers[nm];
+  std::cout << "\n";
+  std::cout << "Numero massimo di esposti : ";
+  std::cout << i.max_e << "\n";
+  std::cout << "Numero massimo di infetti : ";
+  std::cout << i.max_i << "\n\n";
+}
+#endif
+
+
