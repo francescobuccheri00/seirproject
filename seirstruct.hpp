30b9a28ee2e576174cef716c37a3c755214528c8
francescobuccheri00 <69636344+francescobuccheri00@users.noreply.github.com>
2020-08-17 15:59:38 +0200

    Create seirstruct.hpp

---
+++ seirstruct.hpp
@@ -0,0 +1,17 @@
+#ifndef SEIRSTRUCT_HPP
+#define SEIRSTRUCT_HPP
+
+// struttura dati che inglomba le quattro possibili condizioni di salute di un
+// soggetto in tempo di pandemia, il numero di riproduzione di base r0 e il 
+//numero massimo di esposti e infetti toccato durante l'epidemia
+
+struct Seir {
+  double s;
+  double e; // esposti modello seir
+  double i;
+  double r;
+  double r0;
+  double max_e;
+  double max_i;
+};
+#endif
