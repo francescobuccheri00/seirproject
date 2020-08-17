19a9cded18f70e27c0e195553fee27d12b0c3baf
francescobuccheri00 <69636344+francescobuccheri00@users.noreply.github.com>
2020-08-17 16:05:35 +0200

    Add files via upload

---
+++ Derivate.test.cpp
@@ -0,0 +1,29 @@
+#include "derivate.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_CASE("derivate test") {
+Constants k;
+CHECK(k.lam2==50);
+CHECK(k.mi2==0.01);
+CHECK(k.ni2==5);
+CHECK(k.alfa2==0.25);
+CHECK(k.gamma2==0.1);
+//Checking constants to be constants
+
+double S,E,I,R;
+std::cout<<"Controllo della correttezza del coefficiente angolare/andamento delle quattro derivate. Scegli un punto a piacere"<<"\n";
+std::cout<<"Inserire SUSCETTIBILI (di quel punto) : ";
+std::cin>>S;
+std::cout<<"Inserire ESPOSTI (di quel punto) : ";
+std::cin>>E;
+std::cout<<"Inserire INFETTI (di quel punto) : ";
+std::cin>>I;
+std::cout<<"Inserire GUARITI/MORTI (di quel punto) : ";
+std::cin>>R;
+std::cout<<"Di default, si sono considerati: dsdt<0 dedt>0 didt>0 drdt>0. Se si visualizzano messaggi di fallimento, sono da considerarsi i casi opposti"<<"\n";
+CHECK(dsdt(S,I)<0);
+CHECK(dedt(I,S,E)>0);
+CHECK(didt(E,I)>0);
+CHECK(drdt(I,R)>0);
+}
---
+++ Doctest.infomodel.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <cmath>
+
+TEST_CASE("Parameters test") {
+  std::cout << "Nel presente codice verrano chiariti brevemente alcuni "
+               "parametri utilizzati nella funzione main del file seir.cpp"
+            << "\n";
+  double Si = 5000;  // numero di persone suscettibili iniziale preso a scelta
+  double Ei = 4;     // Numero di persone esposte iniziale preso a scelta
+  double beta = 0.1; // probabilità di contagiarsi presa a scelta
+  double ni = 6; // numero medio di persone incontrate nel corso di una giornata
+  double k = 1.331468638;//ricavavto per via calcolistica
+  double Pr = 6 / (10 * Si);//10 dipende dal beta scelto
+  double Pr2 = (beta * ((2 * Si * Ei) / ((Si + Ei) * (Si + Ei - 1))) * (1 / k));
+  CHECK(Pr <= Pr2); // <= per approssimazione
+  std::cout << "Pr : " << Pr << " | Pr2 :" << Pr2 << "\n";
+
+  double tau = 10; // periodo medio della malattia preso a scelta
+  double gamma = 1 / tau;
+  double res = 0;
+  for (int i = 0; i <= 500000; i++) {
+    res += i * pow(1 - gamma, i - 1) * gamma;
+  }
+  CHECK(tau >= res); // >= per approssimazione
+  std::cout << "tau : " << tau << " | tau ricavato come sommatoria : " << res
+            << "\n\n";
+  std::cout << "Formule probabilità : Pr=ni*beta/Si "
+               "Pr2=beta*2*Si*Ei/((Si+Ei)*(Si+Ei-1))*k"
+            << "\n";
+  std::cout << "Formule perodo medio della malattia per soggetto : "
+               "tau=costante tau2=sommatoria per k che va da 0 a un N molto "
+               "grande di k*(1-1/tau)^(k-1) * (1/tau)"
+            << "\n";
+}
+
