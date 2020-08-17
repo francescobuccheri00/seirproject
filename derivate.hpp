d39e508aa3d7d016624e9f39e3c342fbc046c027
francescobuccheri00 <69636344+francescobuccheri00@users.noreply.github.com>
2020-08-17 16:00:16 +0200

    Create derivate.hpp

---
+++ derivate.hpp
@@ -0,0 +1,51 @@
+#ifndef DERIVATE_HPP
+#define DERIVATE_HPP
+
+#include <cassert>
+#include <iostream>
+#include <math.h>
+#include "structconstants.hpp"
+
+
+inline double dsdt(double S, double I) {
+  assert(S > 0 || I >= 0);
+  Constants c;
+  std::cout << "Immetti numero iniziale della popolazione nuovamente : ";
+  double s;
+  std::cin >> s;
+  if (s < 0) {
+    std::cout << "Simpatico! Hai inserito un numero sbagliato, riprovare"
+              << "\n";
+    return 0;
+  }
+  double beta = c.ni2 / (10 * s);
+  return (c.lam2 - (c.mi2 * S) - (beta * I * S));
+}
+
+inline double dedt(double I, double S, double E) {
+  assert(S > 0 || I >= 0 || E > 0);
+  Constants c;
+  std::cout << "Immetti numero iniziale della popolazione nuovamente : "
+            << "\n";
+  double s2;
+  std::cin >> s2;
+  if (s2 < 0) {
+    std::cout << "Simpatico! Hai inserito un numero sbagliato, riprovare";
+    return 0;
+  }
+  double beta = c.ni2 / (10 * s2);
+  return (beta * I * S) - ((c.mi2 + c.alfa2) * E);
+}
+
+inline double didt(double E, double I) {
+  assert(E > 0 || I >= 0);
+  Constants c;
+  return (c.alfa2 * E) - ((c.mi2 + c.gamma2) * I);
+}
+
+inline double drdt(double I, double R) {
+  assert(R >= 0 || I > 0);
+  Constants c;
+  return (c.gamma2 * I) - (c.mi2 * R);
+}
+#endif
