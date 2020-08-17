1a818c09d72eb33a64d8da0cf3f51e35901f5e70
francescobuccheri00 <69636344+francescobuccheri00@users.noreply.github.com>
2020-08-17 16:00:59 +0200

    Create structconstants.hpp

---
+++ structconstants.hpp
@@ -0,0 +1,11 @@
+#ifndef STRUCTCONSTANTS_HPP
+#define STRUCTCONSTANTS_HPP
+
+struct Constants {
+  double lam2 = 50;
+  double mi2 = 0.01;
+  double ni2 = 5;
+  double alfa2 = 0.25; // reciproco del periodo medio di incubazione
+  double gamma2 = 0.1; // reciproco della durata media della malattia
+};
+#endif
