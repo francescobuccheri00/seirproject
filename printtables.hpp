c47084648a69774b64c108b78c071b6186e54532
Francesco Buccheri <69636344+francescobuccheri00@users.noreply.github.com>
2020-08-27 18:57:59 +0200

    Update printtables.hpp

--- printtables.hpp
+++ printtables.hpp
@@ -1,32 +1,32 @@
 #ifndef PRINTTABLES_HPP
 #define PRINTTABLES_HPP
 
-inline void printTables(std::vector<Seir> const &numbers) {
+void printTables(std::vector<Seir> const &numbers) {
   std::cout << "    (S)    "
             << "      (E)     "
             << "   (I)      "
             << "  (R)     "
             << "     R0"
             << "\n";
-  for (auto const &num : numbers) {
-    std::cout << std::to_string(num.s) << " | " << std::to_string(num.e) << " | "
-              << std::to_string(num.i) << " | " << std::to_string(num.r) << " "
-              << " | " << std::to_string(num.r0) << "\n"
+  for (auto const &n : numbers) {
+    std::cout << std::to_string(n.s) << " | " << std::to_string(n.e) << " | "
+              << std::to_string(n.i) << " | " << std::to_string(n.r) << " "
+              << " | " << std::to_string(n.r0) << "\n"
               << "-------------------------------------------------------------"
                  "------------------";
   }
   std::cout << "\n";
   std::cout << "Reinserisci il totale di giorni che hai voluto controllare e "
-               "visualizzerai il numero massimo di esposti e infetti : ";
+               "visualizzerai il numero massimo di infetti : ";
   int nm;
   std::cin >> nm;
-  auto const last = numbers[nm];
+  auto const &i = numbers[nm];
   std::cout << "\n";
-  std::cout << "Numero massimo di esposti : ";
-  std::cout << last.max_e << "\n";
   std::cout << "Numero massimo di infetti : ";
-  std::cout << last.max_i << "\n\n";
+  std::cout << i.max_i << "\n\n";
 }
 #endif
+
+
 
 
