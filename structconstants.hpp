cbb60b6503743d7201c3b00664efbcc0fda8e6e7
Francesco Buccheri <69636344+francescobuccheri00@users.noreply.github.com>
2020-08-28 00:42:29 +0200

    Update structconstants.hpp

--- structconstants.hpp
+++ structconstants.hpp
@@ -2,7 +2,7 @@
 #define STRUCTCONSTANTS_HPP
 
 struct Constants {
-  double lam2 = 50;
+  double lam2 = 10;
   double mi2 = 0.01;
   double ni2 = 5;
   double alfa2 = 0.25; // reciproco del periodo medio di incubazione
